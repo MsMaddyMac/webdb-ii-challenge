28b0d476d93d9c9763381afa76bf92d525740b3d
Maddy <mmcintosh2k14@gmail.com>
2019-12-10 17:28:39 -0500

    creates GET endpoint to retrieve all cars in db

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -6,4 +6,19 @@ const { validateCarId, validateCar } = require('../validators/validators');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Cars.find(req.query)
+    .then(cars => {
+        res
+        .status(200)
+        .json(cars);
+    })
+    .catch(err => {
+        console.log('Issue retrieving cars.', err);
+        res
+        .status(500)
+        .json({ message: 'Error retrieving cars.' })
+    });
+});
+
 module.exports = router;
